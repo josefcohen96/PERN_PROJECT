b20ee6b148151627366efa50b323414cca3ef55c
unknown <you@example.com>
2022-10-12 17:39:09 +0300

    delete input.js

---
+++ client/src/components/TextBox.js
@@ -0,0 +1,10 @@
+import React from 'react';
+function TextBox() {
+    return (
+        <input
+            type="text"
+            className="from-control" />
+
+    )
+}
+export default TextBox;
