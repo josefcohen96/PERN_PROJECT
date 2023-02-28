67060ac634d7214c45131735110e6ec05d031435
josefcohen96 <josef244@gmail.com>
2023-02-28 20:44:43 +0200

    map with saved locations from json file

--- client/src/components/map/MapPage.js
+++ client/src/components/map/MapPage.js
@@ -1,63 +1,45 @@
-// import React, { useEffect, useRef } from 'react';
+// import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 // import L from 'leaflet';
-// import { MapContainer, TileLayer } from 'react-leaflet';
-// import './Map.css';
 // import 'leaflet/dist/leaflet.css';
-
-// // import nationalParks from './national-parks.json';
-
-// delete L.Icon.Default.prototype._getIconUrl;
-
-// // Importing images from locally stored assets to address a bug
-// // in CodeSandbox: https://github.com/codesandbox/codesandbox-client/issues/3845
-
-// L.Icon.Default.mergeOptions({
-//   iconRetinaUrl: require('./images/1.png'),
-//   iconUrl: require('./images/2.png'),
-//   shadowUrl: require('./images/3.png')
-// });
-
-// // When importing into your own app outside of CodeSandbox, you can import directly
-// // from the leaflet package like below
-// //
-// // L.Icon.Default.mergeOptions({
-// //   iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
-// //   iconUrl: require('leaflet/dist/images/marker-icon.png'),
-// //   shadowUrl: require('leaflet/dist/images/marker-shadow.png')
-// // });
-
+// import { useEffect, useRef } from 'react';
+// import icon from "./images/yosef.png";
 // function MapPage() {
-//   const mapRef = useRef();
+//   const mapRef = useRef(null);
+
+//   const myIcon = L.icon({
+//     iconUrl: icon,
+//     iconSize: [40, 40],
+//     iconAnchor: [20, 40],
+//   });
 
 //   useEffect(() => {
-//     const { current = {} } = mapRef;
-//     const { leafletElement: map } = current;
+//     const map = mapRef.current;
 
 //     if (!map) return;
 
 //     const myMarker = L.marker([31.5, 34.9]).addTo(map);
-//     myMarker.bindPopup("<b>Hello world!</b><br>This is my marker.");
-//     myMarker.bringToFront();
-
-
-//     // const parksGeoJson = new L.GeoJSON(nationalParks, {
-//     //   onEachFeature: (feature = {}, layer) => {
-//     //     const { properties = {} } = feature;
-//     //     const { Name } = properties;
-
-//     //     if (!Name) return;
-
-//     //     layer.bindPopup(`<p>${Name}</p>`);
-//     //   }
-//     // });
-
-//     // parksGeoJson.addTo(map);
-//   }, [])
+//     myMarker.bindPopup("<b>Hello world!</b><br>This is my marker.").openPopup();
+//   }, []);
 
 //   return (
 //     <div className="App">
-//       <MapContainer center={[31.5, 34.9]} zoom={10.2}>
-//         <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" attribution="&copy; <a href=&quot;https://www.openstreetmap.org/copyright&quot;>OpenStreetMap</a> contributors" />
+//       <MapContainer
+//         center={[31.5, 34.9]}
+//         zoom={10.2}
+//         style={{ height: '100vh', width: '100%' }}
+//         whenCreated={(map) => (mapRef.current = map)}
+//       >
+//         <TileLayer
+//           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+//           attribution='&copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors'
+//         />
+//         <Marker position={[31.5, 34.9]}icon= {myIcon}>
+//           <Popup>
+//             <b>Hello world!</b>
+//             <br />
+//             This is my marker.
+//           </Popup>
+//         </Marker>
 //       </MapContainer>
 //     </div>
 //   );
@@ -65,52 +47,53 @@
 
 // export default MapPage;
 
+import React, { useEffect, useRef, useState } from 'react';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import L from 'leaflet';
-import 'leaflet/dist/leaflet.css';
-import { useEffect, useRef } from 'react';
+import locationsData from './locations.json';
 import icon from "./images/yosef.png";
+
 function MapPage() {
   const mapRef = useRef(null);
-
-  const myIcon = L.icon({
-    iconUrl: icon,
-    iconSize: [40, 40],
-    iconAnchor: [20, 40],
-  });
+  const [locations, setLocations] = useState([]);
 
   useEffect(() => {
     const map = mapRef.current;
-   
     if (!map) return;
 
-    const myMarker = L.marker([31.5, 34.9]).addTo(map);
-    myMarker.bindPopup("<b>Hello world!</b><br>This is my marker.").openPopup();
-  }, []);
+    const myIcon = L.icon({
+      iconUrl: icon,
+      iconSize: [40, 40],
+      iconAnchor: [20, 40],
+    });
 
+    locations.forEach((location) => {
+      const { name, freq, lastVisit, coordinates} = location;
+      const marker = L.marker(coordinates, { icon: myIcon }).addTo(map);
+      marker.bindPopup(
+        `<b>${name}</b><br />Frequency: ${freq}<br />Last visit: ${lastVisit}`
+      );
+    });
+  }, [locations]);
+
+  useEffect(() => {
+    setLocations(locationsData);
+  }, []);
 
   return (
     <div className="App">
       <MapContainer
         center={[31.5, 34.9]}
         zoom={10.2}
-        style={{ height: '100vh', width: '100%' }}
+        style={{ height: "100vh", width: "100%" }}
         whenCreated={(map) => (mapRef.current = map)}
       >
         <TileLayer
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           attribution='&copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors'
         />
-        <Marker position={[31.5, 34.9]}icon= {myIcon}>
-          <Popup>
-            <b>Hello world!</b>
-            <br />
-            This is my marker.
-          </Popup>
-        </Marker>
       </MapContainer>
     </div>
   );
 }
-
 export default MapPage;
